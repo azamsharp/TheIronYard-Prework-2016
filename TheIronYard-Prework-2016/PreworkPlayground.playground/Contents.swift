7cf01e3cf3635fa0b00a4b22176ede6046be5bf1
azamsharp <azamsharp@gmail.com>
2016-07-31 18:19:55 -0500

    Adding exercises

--- TheIronYard-Prework-2016/PreworkPlayground.playground/Contents.swift
+++ TheIronYard-Prework-2016/PreworkPlayground.playground/Contents.swift
@@ -2,4 +2,28 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// declare a constant called "greeting" and assign initial value "Hello World"
+
+
+
+
+// declare a constant called "a" and assign initial value 10
+
+
+// declare a constant called "b" and assign initial value 10
+
+
+
+// declare a constant called "c" which is a sum of "a" and "b"
+
+
+// declare an array constant called "numbers" and assign with the following initial values
+// [1,2,3,45,23,1,6,7,8]
+
+
+
+// declare an array constant called "names" and assign with the following initial values
+// ["John","Alex","Mary"] 
+
+
+
