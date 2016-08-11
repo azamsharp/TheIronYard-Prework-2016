923843abd3d090e595a55187be026d9e91fe75bd
Mohammad Azam <azamsharp@gmail.com>
2016-08-11 14:27:04 -0500

    Update Contents.swift

--- TheIronYard-Prework-2016/PreworkPlayground.playground/Contents.swift
+++ TheIronYard-Prework-2016/PreworkPlayground.playground/Contents.swift
@@ -2,31 +2,59 @@
 
 import UIKit
 
-// declare a constant called "greeting" and assign initial value "Hello World"
+// declare a constant named city and assign initial value to be "Houston"
+
+// declare a constant named state and assign initial value to be "Texas"
+
+// declare a var named zipcode and assign initial value to be 77042
+
+// declare a constant called firstName and assign its initial value "John"
+
+// declare a constant called lastName and assign its initial value "Doe"
+
+// declare a constant called fullName which is the concatentation of firstName and lastName. Example "John Doe" when appending firstName and lastName together
+
+// use a string literal to print the values of city and temperature
+// Example: The temperature in Houston is 85
+// Use the city and temperature variables declared above in your string literals
 
 
+// create an array called cities and populate with the following "Houston", "Austin", "San Antonio", "Dallas"
+
+// Change the "San Antonio" value to be "Sugarland"
+
+// create a String array called names of populate with the following names
+// "John", "Mary", "Alex","Jack"
+// make sure that the names array can only hold String values
+// The following should not work
+// let names = ["John","Mary","Alex",34]
+
+// iterate through the names array and print out all the names
+
+// iterate through the cities array and print each city name after appending the state name. Example "Houston, TX"
+
+// create a dictionary and initialize it with the following:
+// Dictionary[String:String]
+
+//  key = Houston   value = IAH
+// key = Costa Rica   value = San Jose Airport
+// key = Las Angelos   value = LAX Airport
+
+// print the value of the key "Houston"
+
+// iterate through the dictionary and print all the values of the airports
+
+// create a function called greet which prints "greetings to you" message
+
+// call the function greet
+
+// create a function called add which take two arguments of type int and returns the sum of ints
+
+// call the function add and pass the following arguments (2,5)
+
+// create a class called Person with the following properties
+// firstName :String
+// lastName :String
 
 
-// declare a constant called "a" and assign initial value 10
-
-
-// declare a constant called "b" and assign initial value 10
-
-
-
-// declare a constant called "c" which is a sum of "a" and "b"
-
-
-// declare an array constant called "numbers" and assign with the following initial values
-// [1,2,3,45,23,1,6,7,8]
-
-
-
-// declare an array constant called "names" and assign with the following initial values
-// ["John","Alex","Mary"] 
-
-
-// iterate through the "names" array and print all the contents of the array.
-
-
-
+// create the object of the class Person and assign the properties firstName and lastName with "John" and "Doe"
